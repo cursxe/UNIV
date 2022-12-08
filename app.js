c1c11a4abc75f5d80da000c304a295fb4da8b7b4
Verified ✔ <brianb123b@outlook.com>
2022-12-07 23:06:30 -0500

    update

--- app.js
+++ app.js
@@ -5,12 +5,15 @@ const cors = require('cors')
 const morgan = require('morgan')
 const apiRouter = require('./api')
 
-
 // Setup your Middleware and API Router here
+
 app.use(express.json());
 app.use(cors());
+
 app.use(morgan('dev'));
 app.use('/api', apiRouter);
+
+
 
 
 module.exports = app;
